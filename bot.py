a8dafb39b99b964b6147ec73b46d72cae492b5ea
skdm4135 <143496276+skdm4135@users.noreply.github.com>
2025-12-14 20:50:36 +0530

    Clean up comments and streamline bot execution

--- bot.py
+++ bot.py
@@ -13,16 +13,7 @@
 
 
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
 from main import Bot
-import pyromod.listen  # <--- IMPORTANT: Add this line!
 import TechVJ.speedtest
 
 Bot().run()
-
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
