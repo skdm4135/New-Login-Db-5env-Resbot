38320d0a1df0a666e152fa9b18e0c9de8b29a961
skdm4135 <143496276+skdm4135@users.noreply.github.com>
2025-12-14 20:52:11 +0530

    Clean up main.py by removing comments and empty lines
    Removed commented-out code and redundant lines.

--- main.py
+++ main.py
@@ -1,7 +1,45 @@
+# # Don't Remove Credit Tg - @VJ_Botz
+# # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# # Ask Doubt on telegram @KingVJ01
+
+# from pyrogram import Client
+# from config import API_ID, API_HASH, BOT_TOKEN
+
+# class Bot(Client):
+
+#     def __init__(self):
+#         super().__init__(
+#             "techvj login",
+#             api_id=API_ID,
+#             api_hash=API_HASH,
+#             bot_token=BOT_TOKEN,
+#             plugins=dict(root="TechVJ"),
+#             workers=50,
+#             sleep_threshold=10
+#         )
+
+      
+#     async def start(self):
+            
+#         await super().start()
+#         print('✔️ Bot Started Modified By 𝐖𝐎𝐎𝐃𝐜𝐫𝐚𝐟𝐭')
+
+#     async def stop(self, *args):
+
+#         await super().stop()
+#         print('Bot Stopped Bye')
+
+# # Don't Remove Credit Tg - @VJ_Botz
+# # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# # Ask Doubt on telegram @KingVJ01
+
+
+
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
+import pyromod.listen
 from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
 
@@ -32,10 +70,6 @@ class Bot(Client):
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
-
-
-
-
 
 
 
